81ea183703a17914b0b1e1f346978e09ebea1b0a
SU <s.ulm@divus.biz>
2026-05-06 08:47:20 +0200

    products/issues#2571 Adopted build information

--- git-version.sh
+++ git-version.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 cd $(dirname "$0")
-export DESC=`git describe --abbrev=0`
-export COUNT=`git rev-list $DESC..HEAD --count`
-export HASH=`git rev-parse --short HEAD`
-if [ $COUNT -eq 0 ] ; then 
-	export GIT_VERSION=$DESC
-else
-	export GIT_VERSION=$DESC.$COUNT+$HASH
-fi
+#export DESC=`git describe --abbrev=0`
+#export COUNT=`git rev-list $DESC..HEAD --count`
+#export HASH=`git rev-parse --short HEAD`
+#if [ $COUNT -eq 0 ] ; then
+#	export GIT_VERSION=$DESC
+#else
+#	export GIT_VERSION=$DESC.$COUNT+$HASH
+#fi
+export BRANCH=`git rev-parse --abbrev-ref HEAD`
+export COMMIT=`git rev-parse --short=8 HEAD`
+export BUILDTIME=`date +"%Y%m%d%H%M%S"`
+export GIT_VERSION=`echo "$BRANCH.$COMMIT.$BUILDTIME"`
 echo $GIT_VERSION
 
 # xcrun agvtool new-marketing-version
